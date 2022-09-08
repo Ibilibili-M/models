6015975699d43c9db82ea56f5e2644f2cc05c6fa
lifei <877847812@qq.com>
2022-09-08 10:31:16 +0800

    train: exp-0908修改预训练

--- research/deeplab/wheel_path_config.sh
+++ research/deeplab/wheel_path_config.sh
@@ -2,8 +2,8 @@ ROOT="/home/lifei/models/research/deeplab/datasets/"
 DATA_VERSION="data_v3"
 EXP_NAME="exp-0908"
 
-PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/deeplabv3_pascal_train_aug/model.ckpt"
-# PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/data_v1/exp-0610/train/model.ckpt-800000"
+# PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/deeplabv3_pascal_train_aug/model.ckpt"
+PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/data_v1/exp-0610/train/model.ckpt-800000"
 PATH_TO_DATASET="${ROOT}/${DATA_VERSION}/tfrecord"
 
 PATH_TO_TRAIN_DIR="${ROOT}/${DATA_VERSION}/${EXP_NAME}/train"
