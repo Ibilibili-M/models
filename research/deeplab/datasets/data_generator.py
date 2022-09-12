366c9124c4fa99703834ae3de73d0bfa364e63ed
lee <877847812@qq.com>
2022-09-12 23:34:28 +0800

    train: data_v4_0912

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -110,8 +110,8 @@ _ADE20K_INFORMATION = DatasetDescriptor(
 
 _WHEEL_INFORMATION = DatasetDescriptor(
     splits_to_sizes={
-        'train': 26619,  # num of samples in images/training
-        'val': 269,  # num of samples in images/validation
+        'train': 30581,  # num of samples in images/training
+        'val': 1610,  # num of samples in images/validation
     },
     num_classes=13,
     ignore_label=255,
