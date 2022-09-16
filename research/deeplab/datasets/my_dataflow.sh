b7bc0d0f3063ad0ef48668b949dc3cbc8b159930
lifei <877847812@qq.com>
2022-09-16 16:39:18 +0800

    mod: 修改dataflow

--- research/deeplab/datasets/my_dataflow.sh
+++ research/deeplab/datasets/my_dataflow.sh
@@ -1,6 +1,6 @@
 set -e
 
-version="_v4"
+version="_v5"
 data_labelme="./data_annotated"${version}
 data_voc="./data_dataset_voc"${version}
 echo "data_labelme:"${data_labelme}"data_voc:"${data_voc}
@@ -11,24 +11,21 @@ echo "data_labelme:"${data_labelme}"data_voc:"${data_voc}
 # --output_dir=${data_voc}"/SegmentationClassPNG-raw"
 
 DATASET_NAME="data"${version}
-# echo "dataset_name:"${DATASET_NAME}
-# mkdir $(pwd)"/"${DATASET_NAME}
-# mkdir $(pwd)"/"${DATASET_NAME}"/image"
-# mkdir $(pwd)"/"${DATASET_NAME}"/index"
-# mkdir $(pwd)"/"${DATASET_NAME}"/mask"
-# mkdir $(pwd)"/"${DATASET_NAME}"/test"
-# mkdir $(pwd)"/"${DATASET_NAME}"/testannot"
-# mkdir $(pwd)"/"${DATASET_NAME}"/tfrecord"
-# mkdir $(pwd)"/"${DATASET_NAME}"/train"
-# mkdir $(pwd)"/"${DATASET_NAME}"/trainannot"
-# mkdir $(pwd)"/"${DATASET_NAME}"/val"
-# mkdir $(pwd)"/"${DATASET_NAME}"/valannot"
+echo "dataset_name:"${DATASET_NAME}
+mkdir $(pwd)"/"${DATASET_NAME}
+mkdir $(pwd)"/"${DATASET_NAME}"/image"
+mkdir $(pwd)"/"${DATASET_NAME}"/index"
+mkdir $(pwd)"/"${DATASET_NAME}"/mask"
+mkdir $(pwd)"/"${DATASET_NAME}"/test"
+mkdir $(pwd)"/"${DATASET_NAME}"/testannot"
+mkdir $(pwd)"/"${DATASET_NAME}"/tfrecord"
+mkdir $(pwd)"/"${DATASET_NAME}"/train"
+mkdir $(pwd)"/"${DATASET_NAME}"/trainannot"
+mkdir $(pwd)"/"${DATASET_NAME}"/val"
+mkdir $(pwd)"/"${DATASET_NAME}"/valannot"
 
-# # cp -r ${data_voc}"/JPEGImages/*" $(pwd)"/"${DATASET_NAME}"/image"
-# # cp -r ${data_voc}"/SegmentationClassPNG-raw/*" $(pwd)"/"${DATASET_NAME}"/mask"
-
-# find ./data_dataset_voc_v4/JPEGImages/ -name "*.*" |xargs -i cp {} ./data_v4/image
-# find ./data_dataset_voc_v4/SegmentationClassPNG-raw/ -name "*.*" |xargs -i cp {} ./data_v4/mask
+find ${data_voc}/JPEGImages/ -name "*.*" |xargs -i cp {} $(pwd)/${DATASET_NAME}/image
+find ${data_voc}/SegmentationClassPNG-raw/ -name "*.*" |xargs -i cp {} $(pwd)/${DATASET_NAME}/mask
 
 # python get_train_val.py
 
