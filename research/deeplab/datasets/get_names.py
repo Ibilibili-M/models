bb57c3300aa8899252f15a769dabbb13ef6c39c5
lifei <lifei@crf-tech.com>
2022-10-11 16:19:47 +0800

    mod:修改数据集划分工具

--- research/deeplab/datasets/get_names.py
+++ research/deeplab/datasets/get_names.py
@@ -1,47 +1,62 @@
-import os,shutil
+import os
+import shutil
+import argparse
 from PIL import Image
  
- 
- 
-train_path = '/home/lifei/models/research/deeplab/datasets/data_v4/train/'
-filelist_train = sorted(os.listdir(train_path))
-val_path = '/home/lifei/models/research/deeplab/datasets/data_v4/val/'
-filelist_val = sorted(os.listdir(val_path))
-test_path = '/home/lifei/models/research/deeplab/datasets/data_v4/test/'
-filelist_test = sorted(os.listdir(test_path))
-index_path = '/home/lifei/models/research/deeplab/datasets/data_v4/index/'
- 
-VOC_file_dir = index_path
- 
- 
-VOC_train_file = open(os.path.join(VOC_file_dir, "train.txt"), 'w')
-VOC_val_file = open(os.path.join(VOC_file_dir, "val.txt"), 'w')
-VOC_test_file = open(os.path.join(VOC_file_dir, "trainval.txt"), 'w')
-VOC_train_file.close()
-VOC_val_file.close()
-VOC_test_file.close()
- 
-VOC_train_file = open(os.path.join(VOC_file_dir, "train.txt"), 'a')
-VOC_val_file = open(os.path.join(VOC_file_dir, "val.txt"), 'a')
-VOC_test_file = open(os.path.join(VOC_file_dir, "trainval.txt"), 'a')
- 
-for eachfile in filelist_train:
-    (temp_name,temp_extention) = os.path.splitext(eachfile)
-    img_name = temp_name
-    VOC_train_file.write(img_name + '\n')
- 
-for eachfile in filelist_val:
-    (temp_name, temp_extention) = os.path.splitext(eachfile)
-    img_name = temp_name
-    VOC_val_file.write(img_name + '\n')
- 
-for eachfile in filelist_test:
-    (temp_name, temp_extention) = os.path.splitext(eachfile)
-    img_name = temp_name
-    VOC_test_file.write(img_name + '\n')
- 
-VOC_train_file.close()
-VOC_val_file.close()
-VOC_test_file.close()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str, default='', help='')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    data_root = os.path.abspath(args.data_root)
+    train_path = data_root + '/train/'
+    filelist_train = sorted(os.listdir(train_path))
+    val_path = data_root + '/val/'
+    filelist_val = sorted(os.listdir(val_path))
+    test_path = data_root + '/test/'
+    filelist_test = sorted(os.listdir(test_path))
+    index_path = data_root + '/index/'
+
+    VOC_file_dir = index_path
+
+    VOC_train_file = open(os.path.join(VOC_file_dir, "train.txt"), 'w')
+    VOC_val_file = open(os.path.join(VOC_file_dir, "val.txt"), 'w')
+    VOC_test_file = open(os.path.join(VOC_file_dir, "trainval.txt"), 'w')
+    VOC_train_file.close()
+    VOC_val_file.close()
+    VOC_test_file.close()
+
+    VOC_train_file = open(os.path.join(VOC_file_dir, "train.txt"), 'a')
+    VOC_val_file = open(os.path.join(VOC_file_dir, "val.txt"), 'a')
+    VOC_test_file = open(os.path.join(VOC_file_dir, "trainval.txt"), 'a')
+
+    for eachfile in filelist_train:
+        (temp_name, temp_extention) = os.path.splitext(eachfile)
+        img_name = temp_name
+        VOC_train_file.write(img_name + '\n')
+
+    for eachfile in filelist_val:
+        (temp_name, temp_extention) = os.path.splitext(eachfile)
+        img_name = temp_name
+        VOC_val_file.write(img_name + '\n')
+
+    for eachfile in filelist_test:
+        (temp_name, temp_extention) = os.path.splitext(eachfile)
+        img_name = temp_name
+        VOC_test_file.write(img_name + '\n')
+
+    VOC_train_file.close()
+    VOC_val_file.close()
+    VOC_test_file.close()
+
+
+if __name__ == '__main__':
+    main()
+
  
  
--- research/deeplab/datasets/get_train_val.py
+++ research/deeplab/datasets/get_train_val.py
@@ -1,45 +1,64 @@
 #!/usr/bin/env python
 import numpy as np
-import os, sys
+import os
+import sys
+import argparse
 import os.path
 from PIL import Image
-###############################
-#处理原图
-url = '/home/lifei/models/research/deeplab/datasets/data_v4/'#图片存储的文件夹名称
-src_url = url + 'image/'
-mask_url = url + 'mask/'
-pathlist=os.listdir(src_url)
-n=len(pathlist)#图片的数量
-array = np.arange(n)#产生长度为n的序列
-np.random.shuffle(array)#将arrray序列随机排列
-#把path文件夹下以及其子文件下的所有.jpg图片移动到new_path文件夹下
-def moveImg(path,new_path):
-    img=Image.open(path)
-    img.save(os.path.join(new_path,os.path.basename(path)))
-#30%的数据生成验证集
-src_new_path = url + 'val/'
-mask_new_path = url + 'valannot/'
-i=0
-while(i <= (int(n*0.05))):
-    src_str = pathlist[array[i]]    
-    DatasetPath = src_url  + src_str
-    print('path: ',DatasetPath)
-    moveImg(DatasetPath,src_new_path)
-    mask_str = src_str.replace("jpg","png")
-    DatasetPath = mask_url  + mask_str
-    print('path: ',DatasetPath)
-    moveImg(DatasetPath,mask_new_path)
-    i=i+1
-#70%的数据生成训练集
-src_new_path = url + 'train/'
-mask_new_path = url + 'trainannot/'
-while(i <= (n-1)):
-    src_str = pathlist[array[i]] 
-    DatasetPath = src_url + src_str
-    print('path: ',DatasetPath)
-    moveImg(DatasetPath,src_new_path)
-    mask_str = src_str.replace("jpg","png")
-    DatasetPath = mask_url  + mask_str
-    print('path: ',DatasetPath)
-    moveImg(DatasetPath,mask_new_path)
-    i=i+1    
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str, default='', help='')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    data_root = os.path.abspath(args.data_root)
+    ###############################
+    #处理原图
+    url = data_root + '/'  #图片存储的文件夹名称
+    src_url = url + 'image/'
+    mask_url = url + 'mask/'
+    pathlist=os.listdir(src_url)
+    n=len(pathlist)#图片的数量
+    array = np.arange(n)#产生长度为n的序列
+    np.random.shuffle(array)#将arrray序列随机排列
+    #把path文件夹下以及其子文件下的所有.jpg图片移动到new_path文件夹下
+    def moveImg(path,new_path):
+        img=Image.open(path)
+        img.save(os.path.join(new_path,os.path.basename(path)))
+    #30%的数据生成验证集
+    src_new_path = url + 'val/'
+    mask_new_path = url + 'valannot/'
+    i=0
+    while(i <= (int(n*0.05))):
+        src_str = pathlist[array[i]]
+        DatasetPath = src_url  + src_str
+        print('path: ',DatasetPath)
+        moveImg(DatasetPath,src_new_path)
+        mask_str = src_str.replace("jpg","png")
+        DatasetPath = mask_url  + mask_str
+        print('path: ',DatasetPath)
+        moveImg(DatasetPath,mask_new_path)
+        i=i+1
+    #70%的数据生成训练集
+    src_new_path = url + 'train/'
+    mask_new_path = url + 'trainannot/'
+    while(i <= (n-1)):
+        src_str = pathlist[array[i]]
+        DatasetPath = src_url + src_str
+        print('path: ',DatasetPath)
+        moveImg(DatasetPath,src_new_path)
+        mask_str = src_str.replace("jpg","png")
+        DatasetPath = mask_url  + mask_str
+        print('path: ',DatasetPath)
+        moveImg(DatasetPath,mask_new_path)
+        i=i+1
+
+
+if __name__ == '__main__':
+    main()
+
