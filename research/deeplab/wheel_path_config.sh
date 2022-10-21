a077ae3c8182d19d8c0be770606988f58f218226
lee <877847812@qq.com>
2022-10-21 17:43:53 +0800

    train: exp-1021-custome-pretrain

--- research/deeplab/wheel_path_config.sh
+++ research/deeplab/wheel_path_config.sh
@@ -1,12 +1,13 @@
 ROOT="/home/lifei/models/research/deeplab/datasets/"
-DATA_VERSION="data_v6"
-EXP_NAME="exp-0921"
+DATA_VERSION="data_v12"
+EXP_NAME="exp-1021_pretrain-1020"
 
 # PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/deeplabv3_pascal_train_aug/model.ckpt"
-PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/data_v6/exp-0916/train/model.ckpt-500000"
+# PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/data_v11-v2/exp-1013/train/model.ckpt-800000"
+PATH_TO_INITIAL_CHECKPOINT="/home/lifei/models/research/deeplab/datasets/data_v12/exp-1020/train/model.ckpt-300000"
 PATH_TO_DATASET="${ROOT}/${DATA_VERSION}/tfrecord"
 
 PATH_TO_TRAIN_DIR="${ROOT}/${DATA_VERSION}/${EXP_NAME}/train"
-PATH_TO_EVAL_DIR="${ROOT}/${DATA_VERSION}/${EXP_NAME}/eval"
+PATH_TO_EVAL_DIR="${ROOT}/${DATA_VERSION}/eval/${EXP_NAME}"
 PATH_TO_VIS_DIR="${ROOT}/${DATA_VERSION}/${EXP_NAME}/vis"
 EXPORT_DIR="${ROOT}/${DATA_VERSION}/${EXP_NAME}/export"
--- research/deeplab/wheel_tensorboard.sh
+++ research/deeplab/wheel_tensorboard.sh
@@ -1,1 +1,3 @@
-tensorboard --logdir="./exp"
+. ./wheel_path_config.sh
+
+tensorboard --logdir=${PATH_TO_EVAL_DIR}
